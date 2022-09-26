3b9378581da94cc00026228f8141e160e2b0aef9
devmagrfs <devmagrfs@gmail.com>
2022-09-26 23:37:44 +0900

    style: video 정보가 없을 시 play 버튼 클릭 못하게 수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,6 +5,8 @@ import requests from '../api/requests';
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
+  const [isVideo, setIsVideo] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -15,39 +17,57 @@ const Banner = () => {
     const movieId = request.data.results[Math.floor(Math.random() * request.data.results.length)].id;
 
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
-      params: { append_to_response: "video" },
+      params: { append_to_response: 'video' },
     });
 
     setMovie(movieDetail);
+    setIsVideo(!movieDetail.video);
   };
 
   const truncate = (str, n) => {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   };
 
-  return (
-    <BannerContainer style={{
-      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-      backgroundPosition: 'top center',
-      backgroundSize: 'cover',
-    }}>
-      <BannerContents>
-        <BannerTitle>
-          {movie.title || movie.name || movie.original_name}
-        </BannerTitle>
-        <BannerButtons>
-          <BannerPlayButton>
-            Play
-          </BannerPlayButton>
-          <BannerInfoButton>
-            More Information
-          </BannerInfoButton>
-        </BannerButtons>
-        <BannerDescription>{truncate(movie.overview, 100)}</BannerDescription>
-      </BannerContents>
-      <BannerFadeBottom></BannerFadeBottom>
-    </BannerContainer>
-  )
+  if (!isClicked) {
+    return (
+      <BannerContainer style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: 'top center',
+        backgroundSize: 'cover',
+      }}>
+        <BannerContents>
+          <BannerTitle>
+            {movie.title || movie.name || movie.original_name}
+          </BannerTitle>
+          <BannerButtons>
+            <BannerPlayButton onClick={() => setIsClicked(true)} disabled={isVideo}>
+              Play
+            </BannerPlayButton>
+            <BannerInfoButton>
+              More Information
+            </BannerInfoButton>
+          </BannerButtons>
+          <BannerDescription>{truncate(movie.overview, 100)}</BannerDescription>
+        </BannerContents>
+        <BannerFadeBottom></BannerFadeBottom>
+      </BannerContainer>
+    )
+  } else {
+    return (
+      <VideoContainer>
+        <VideoHomeContainer>
+          <Iframe
+            src={`https://www.youtube.com/embed/${movie.videos?.results[0]?.key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos?.results[0]?.key}`}
+            title='Youtube Video Player'
+            width='640'
+            height='360'
+            frameborder='0'
+            allow='autoplay; fullscreen'
+          ></Iframe>
+        </VideoHomeContainer>
+      </VideoContainer>
+    )
+  }
 }
 
 const BannerContainer = styled.header`
@@ -113,6 +133,10 @@ const BannerButton = styled.button`
 const BannerPlayButton = styled(BannerButton)`
   background-color: white;
   color: black;
+
+  &:disabled {
+    cursor: default;
+  }
 `;
 
 const BannerInfoButton = styled(BannerButton)`
@@ -159,6 +183,37 @@ const BannerFadeBottom = styled.div`
     bottom: 0;
     width: 100%;
     height: 40rem;
+  }
+`;
+
+const VideoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const VideoHomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
   }
 `;
 
