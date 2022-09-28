0d34974d7aa972358d4fda6aabc45a8289a4f516
devmagrfs <devmagrfs@gmail.com>
2022-09-28 16:08:05 +0900

    style: searchPage 스타일링

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import styled from 'styled-components';
 
 import axios from '../../api/axios';
 
@@ -33,34 +34,71 @@ const SearchPage = () => {
 
   const renderSearchResults = () => {
     return searchResults.length > 0 ? (
-      <section className='search-container'>
+      <ResultSearchContainer className='search-container'>
         {searchResults.map((movie) => {
           if (movie.backdrop_path !== null && movie.media_type !== 'person') {
             const movieImageUrl = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path
 
             return (
-              <div className='movie'>
-                <div className='movie__column-poster'>
-                  <img src={movieImageUrl} alt="movie" className='movie__poster' />
-                </div>
-              </div>
+              <SearchMovieBox key={movie.id}>
+                <SearchMoviePoster>
+                  <SearchMovieImage src={movieImageUrl} alt="movie" />
+                </SearchMoviePoster>
+              </SearchMovieBox>
             )
           }
         })}
-      </section>
+      </ResultSearchContainer>
     ) : (
-      <section className='no-results'>
+      <NoResultSearchContainer>
         <div className='no-results__text'>
           <p>
-            찾고자 하는 검색어 `${searchTerm}`에 맞는 영화가 없습니다.
+            찾고자 하는 검색어 `{searchTerm}`에 맞는 영화가 없습니다.
           </p>
         </div>
-      </section>
+      </NoResultSearchContainer>
     )
-  }
+  };
 
   return renderSearchResults();
 }
 
+const ResultSearchContainer = styled.section`
+  background: black;
+  width: 100%;
+  text-align: center;
+  padding: 5rem 0;
+`;
+
+const NoResultSearchContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #c5c5c5;
+  height: 100%;
+  padding: 8rem;
+`;
+
+const SearchMovieBox = styled.div`
+  flex: 1 1 auto;
+  display: inline-block;
+  padding-right: 0.5rem;
+  padding-bottom: 7rem;
+`
+
+const SearchMoviePoster = styled.div`
+  cursor: pointer;
+  transition: transform 0.3s;
+  -webkit-transition: transform 0.3s;
+
+  &:hover {
+    transform: scale(1.25);
+  }
+`;
+
+const SearchMovieImage = styled.img`
+  width: 90%;
+  border-radius: 5px;
+`;
 
 export default SearchPage;
